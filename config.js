21d4ab0d491bb53bbe00f0c96a7e1b4a160ad5b9
Moritz Warning <moritzwarning@web.de>
2015-05-29 21:14:30 +0200

    fix JS code formatting

--- config.js
+++ config.js
@@ -1,29 +1,32 @@
 /* after making any changes here enter "make" in your terminal to appy the changes */
-
 var ffmapConfig = {
-  // link to your main community site:
-  url:       "/",
+    // link to your main community site:
+    url: "/",
 
-  // visible link in the navigation:
-  community_site : "gotham.freifunk.net",
-  community_name : "Freifunk Gotham",
+    // visible link in the navigation:
+    community_site: "gotham.freifunk.net",
+    community_name: "Freifunk Gotham",
 
-  // ipv6 prefix for link to router status site
-  prefix6 : "fdef:17a0:ffb1:300::",
+    // ipv6 prefix for link to router status site
+    prefix6: "fdef:17a0:ffb1:300::",
 
-  // initial gravity, friction, of the graph at pageload:
-  gravity:   0.05,
-  friction:  0.73,
-  theta:     0.8,
-  charge:    1.0,
-  distance:  1.0,
-  strength:  1.0,
+    // initial gravity, friction, of the graph at pageload:
+    gravity: 0.05,
+    friction: 0.73,
+    theta: 0.8,
+    charge: 1.0,
+    distance: 1.0,
+    strength: 1.0,
 
-  // all communities to be listed
-  community_labels : { "gotham" : "Gotham" },
-  community_links: { "gotham" : "http://gotham.freifunk.net" },
-  show_unlisted_communities : true,
+    // all communities to be listed
+    community_labels: {
+        "gotham": "Gotham"
+    },
+    community_links: {
+        "gotham": "http://gotham.freifunk.net"
+    },
+    show_unlisted_communities: true,
 
-  // path to the nodes.json
-  nodes_json: "nodes.json",
+    // path to the nodes.json
+    nodes_json: "nodes.json",
 };
